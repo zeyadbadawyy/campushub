3601ab6d32fafc352005a95694fb76158f2b35ea
Zeyad Badawy <zeyadbadawyy@gmail.com>
2026-08-21 21:05:58 +0300

    add / route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,8 @@
 import {
   BrowserRouter,
   Routes,
-  Route
+  Route,
+  Navigate
 } from "react-router-dom";
 
 import Dashboard from "./pages/Dashboard";
@@ -27,6 +28,11 @@ function App() {
     <BrowserRouter>
 
       <Routes>
+
+        <Route 
+          path="/" 
+          element={<Navigate to="/login" replace />} 
+        />
 
         <Route
           path="/login"

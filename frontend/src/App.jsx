579d37400514c31576d3c00b096dcc918fdf3540
Zeyad Badawy <zeyadbadawyy@gmail.com>
2026-08-22 22:47:21 +0300

    fix logo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -31,7 +31,7 @@ function App() {
 
         <Route 
           path="/" 
-          element={<Navigate to="/login" replace />} 
+          element={<Navigate to="/register" replace />} 
         />
 
         <Route
